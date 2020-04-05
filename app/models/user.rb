95ef680f91284a7f5bfbc1ad2bf96a9db96534c6
github username <github email address>
2020-04-05 14:55:46 -0700

    validation and secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ActiveRecord::Base
 
     has_many :pokemon
 
+    validates :username, presence: true, uniqueness: true
+    validates :email,  presence: true, uniqueness: true
+
+    has_secure_password
+
 end
