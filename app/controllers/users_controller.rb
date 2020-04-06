74dcd143c8767061fa1c466fd65bb80c52e7dbe5
github username <github email address>
2020-04-06 16:39:21 -0700

    edited user controller and views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,19 @@
 class UsersController < ApplicationController
 
+    get '/signup' do
+        erb :'users/signup'
+    end
+
+    post '/users' do
+        user = User.create(params) #the @user instance variable would only be needed if you need to access it in the view
+        redirect to "/users/#{user.id}"
+    end
+
+    get '/user/:id' do #:id makes this a dynamic route
+        @user = User.find_by(id: params[:id])
+        erb :'/users/show'
+    end
+
+
+
 end
