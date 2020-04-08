9363b4d49f67adf94c28e831006bf9dd97a3b1f8
github username <github email address>
2020-04-07 21:35:47 -0700

    updating pokemon controller

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -5,7 +5,10 @@ class PokemonController < ApplicationController
     end
 
     post '/pokemon' do
-
+        pokemon = Pokemon.create(params)
+        user = Helpers.current_user(session)
+        pokemon.user = user
+        redirect to "/users/#{user.id}"
     end
-    
+
 end
