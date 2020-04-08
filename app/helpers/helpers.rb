f36ebac5631b1cf96b0c7eb71b537c8ebf1711d3
github username <github email address>
2020-04-07 19:52:42 -0700

    working on helpers

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -4,8 +4,8 @@ class Helpers
         User.find_by(id: session[:user_id])
     end
 
-    def self.logged_in?(session)
-        !!session[:user_id]
+    def self.is_logged_in?(session)
+        session[:user_id] ? true : false
     end
 
 end
